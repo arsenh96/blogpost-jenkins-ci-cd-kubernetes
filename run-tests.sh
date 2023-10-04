9615da7a78d47e802a8344f29749331ae713e9e5
Arsen Harutjunjan <a.harutjunjan@student.han.nl>
2023-10-05 00:57:15 +0200

    bat file made from wsl in jenkinsfile

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,5 @@
 # Navigeer naar de testmap
-cd /mnt/c/src/minor_devops/blogpost-jenkins-ci-cd-kubernetes/HelloWorldWebApp.Tests/
+cd C:\src\minor_devops\blogpost-jenkins-ci-cd-kubernetes/HelloWorldWebApp.Tests/
 
 # Voer de tests uit
 dotnet test
