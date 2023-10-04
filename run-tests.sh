07d0ebe4e3e059bfe7e099508b9eb77215491519
Arsen Harutjunjan <a.harutjunjan@student.han.nl>
2023-10-05 00:14:48 +0200

    test file fixed

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Navigeer naar de testmap
-cd '/mnt/c/src/minor_devops/blogpost-jenkins-ci-cd-kubernetes/HelloWorldWebApp.Tests/'
+cd /c/src/minor_devops/blogpost-jenkins-ci-cd-kubernetes/HelloWorldWebApp.Tests/
 
 # Voer de tests uit
 dotnet test
